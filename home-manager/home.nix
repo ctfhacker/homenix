2e6b3167c7e4b81a7cc3e85da15ffdfa575fa7ec
Cory Duplantis <cld251@gmail.com>
2023-12-28 10:39:19 -0600

    Init nix config

---
+++ home-manager/home.nix
@@ -0,0 +1,242 @@
+{ config, pkgs, lib, ... }:
+
+let
+  i3_mod = "Mod1"; # Left Alt/Option
+in {
+  # Use home manager
+  programs.home-manager.enable = true;
+
+  home.username = "user";
+  home.homeDirectory = "/home/user";
+  home.stateVersion = "23.11";
+  home.packages = with pkgs; [
+    bat
+    firefox
+    helix
+    hexyl
+    htop
+    less
+    radare2
+    ripgrep
+    unzip
+    xxd
+    zip
+  ];
+
+  home.file.".inputrc".text = ''
+  set editing-mode vi
+  set keymap vi-command
+  # set show-mode-in-prompt on
+  $if mode=vi
+    # Normal mode
+    set keymap vi-command
+    "H": beginning-of-line
+    "L": end-of-line
+    "j": history-search-forward
+    "k": history-search-backward
+    set keymap vi-insert
+    "jk": vi-movement-mode
+  $endif
+  set expand-tilde on
+  '';
+ 
+  xsession.windowManager.i3 = {
+    enable = true;
+    config = {
+      modifier = i3_mod;
+
+      keybindings = lib.mkOptionDefault {
+        # Focus
+        "${i3_mod}+h" = "focus left";
+        "${i3_mod}+j" = "focus down";
+        "${i3_mod}+k" = "focus up";
+        "${i3_mod}+l" = "focus right";
+
+        # Move
+        "${i3_mod}+Shift+h" = "move left";
+        "${i3_mod}+Shift+j" = "move down";
+        "${i3_mod}+Shift+k" = "move up";
+        "${i3_mod}+Shift+l" = "move right";
+      };
+
+      bars = [
+        {
+          position = "bottom";
+          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-bottom.toml";
+        }
+       ];
+    };
+  };
+
+  ####################################################################
+  ### Programs to enable                                           ###
+  ####################################################################
+
+  programs.alacritty.enable = true;
+
+  programs.autorandr = {
+    enable = true;
+
+    profiles = {
+      "default" = {
+        fingerprint = {
+          "DP-5" = "00ffffffffffff005a63363201010101071f0104a5351e783f05f5a557529c270b5054bfef80b300a940a9c0950090408180814081c0023a801871382d40582c45000f282100001e000000ff00564c383231303741303633340a000000fd00304b535311010a202020202020000000fc005641323435362053657269657301e002031af14d010203040590121113141e1d1f23097f07830100006842806a70382740082098040f282100001e011d8018711c1620582c25000f282100009e023a80d072382d40102c45800f282100001e011d007251d01e206e2855000f282100001e011d00bc52d01e20b82855400f282100001e0000000000000000000000a6";
+          "DP-6" = "00ffffffffffff005a63363201010101071f0104a5351e783f05f5a557529c270b5054bfef80b300a940a9c0950090408180814081c0023a801871382d40582c45000f282100001e000000ff00564c383231303741303539310a000000fd00304b535311010a202020202020000000fc005641323435362053657269657301de02031af14d010203040590121113141e1d1f23097f07830100006842806a70382740082098040f282100001e011d8018711c1620582c25000f282100009e023a80d072382d40102c45800f282100001e011d007251d01e206e2855000f282100001e011d00bc52d01e20b82855400f282100001e0000000000000000000000a6";
+          "DP-7" = "00ffffffffffff005a63363201010101071f0104a5351e783f05f5a557529c270b5054bfef80b300a940a9c0950090408180814081c0023a801871382d40582c45000f282100001e000000ff00564c383231303741303538380a000000fd00304b535311010a202020202020000000fc005641323435362053657269657301d802031af14d010203040590121113141e1d1f23097f07830100006842806a70382740082098040f282100001e011d8018711c1620582c25000f282100009e023a80d072382d40102c45800f282100001e011d007251d01e206e2855000f282100001e011d00bc52d01e20b82855400f282100001e0000000000000000000000a6";
+          "DP-8" = "00ffffffffffff005a63363201010101071f0104a5351e783f05f5a557529c270b5054bfef80b300a940a9c0950090408180814081c0023a801871382d40582c45000f282100001e000000ff00564c383231303741303630370a000000fd00304b535311010a202020202020000000fc005641323435362053657269657301e002031af14d010203040590121113141e1d1f23097f07830100006842806a70382740082098040f282100001e011d8018711c1620582c25000f282100009e023a80d072382d40102c45800f282100001e011d007251d01e206e2855000f282100001e011d00bc52d01e20b82855400f282100001e0000000000000000000000a6";
+        };
+        config = {
+          "DP-1-1" = {
+            enable = false;
+          };
+          "DP-1-2" = {
+            enable = false;
+          };          
+          "DP-1-3" = {
+            enable = false;
+          };
+          "DP-1-4" = {
+            enable = false;
+          };
+          "DP-5" = {
+            enable = true;
+            crtc = 0;
+            mode = "1920x1080";
+            primary = true;
+            rate = "60.00";
+            position = "0x1080";
+          };
+          "DP-6" = {
+            enable = true;
+            crtc = 1; 
+            mode = "1920x1080";
+            rate = "60.00";
+            position = "0x0";
+            rotate = "inverted";
+          };
+          "DP-7" = {
+            enable = true;
+            crtc = 2; 
+            mode = "1920x1080";
+            rate = "60.00";
+            position = "1920x0";
+            rotate = "left";      
+          };
+          "DP-8" = {
+            enable = true;
+            crtc = 3; 
+            mode = "1920x1080";
+            rate = "60.00";
+            position = "3000x0";
+            rotate = "right";          
+          };
+        };
+      };
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    bashrcExtra = ''
+    '';
+    shellAliases = {
+      "vim" = "hx";
+      ".." = "cd ..";
+      "..." = "cd ../..";
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+    userName = "Cory Duplantis";
+    userEmail = "cld251@gmail.com";
+    aliases = {
+      s = "status";
+      co = "checkout";
+      cob = "checkout -b";
+    };
+  };
+
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "solarized_dark";
+      editor = {
+        line-number = "relative";
+      };
+    };
+  };
+
+
+  programs.i3status-rust = {
+    enable = true;
+    bars = {
+      bottom = {
+        theme = "solarized-dark";
+        icons = "none";
+        blocks = [
+          {
+            block = "sound";
+          }
+          {
+            block = "cpu";
+            interval = 1;
+            format = "$icon $barchart $utilization";
+          }
+          {
+            block = "memory";
+            interval = 5;
+            format = "$icon $mem_used_percents";
+          }
+          {
+            block = "disk_space";
+            warning = 15.0;
+            alert = 10.0;
+            alert_unit = "GB";
+            interval = 30;
+            format = "$icon $available";
+          }
+          {
+            block = "net";
+            interval = 10;
+            format = "$icon $ip";
+          }
+          {
+            block = "time";
+            interval = 1;
+            format = "$icon $timestamp.datetime(f:'%a %Y/%m/%d')";
+          }
+        ];
+      };
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+  };
+
+  programs.tmux = {
+    enable = true;
+    prefix = "C-a";
+    secureSocket = true; # Sets socket in /run instead of /tmp
+    escapeTime = 0;
+    aggressiveResize = true;
+    historyLimit = 256000;
+    keyMode = "vi";
+    extraConfig = ''
+    set -g repeat-time 0
+
+    bind h select-pane -L
+    bind j select-pane -D
+    bind k select-pane -U
+    bind l select-pane -R
+
+    # Allows to use C-a a <command> to send commands to a nested TMUX
+    bind-key a send-prefix
+    bind-key - split-window
+    bind-key \\ split-window -h
+    '';
+  };
+
+  }
