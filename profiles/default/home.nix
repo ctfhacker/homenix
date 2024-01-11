c549348959d638c94890785cab53298134d651e8
Cory Duplantis <cld251@gmail.com>
2024-01-11 17:06:00 -0600

    Add utilities for pulling new nix config on non-nixos boxes

--- profiles/default/home.nix
+++ profiles/default/home.nix
@@ -175,6 +175,8 @@ in {
       "tree" = "lsd -la --tree";
       "goto_mac" = "for b in 35 36; do sudo ddcutil --bus $b setvcp 60 17; done";
       "goto_nix" = "for b in 35 36; do sudo ddcutil --bus $b setvcp 60 15; done";
+      "pull_nix" = "cd ~/homenix ; git pull ; home-manager switch --flake .#user";
+      "nixnix" = "source $HOME/.nix-profile/etc/profile.d/nix.sh";
     };
   };
 
