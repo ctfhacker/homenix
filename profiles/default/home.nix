6efc65ef5cbafac8fd79e5d1070170d2fa58f8ba
Cory Duplantis <cld251@gmail.com>
2024-01-11 17:09:14 -0600

    Typo in pull_nix alias

--- profiles/default/home.nix
+++ profiles/default/home.nix
@@ -175,7 +175,7 @@ in {
       "tree" = "lsd -la --tree";
       "goto_mac" = "for b in 35 36; do sudo ddcutil --bus $b setvcp 60 17; done";
       "goto_nix" = "for b in 35 36; do sudo ddcutil --bus $b setvcp 60 15; done";
-      "pull_nix" = "popd ~/homenix ; git stash ; git pull ; git stash apply ; home-manager switch --flake .#user ; popd";
+      "pull_nix" = "pushd ~/homenix ; git stash ; git pull ; git stash apply ; home-manager switch --flake .#user ; popd";
       "nixnix" = "source $HOME/.nix-profile/etc/profile.d/nix.sh";
     };
   };
