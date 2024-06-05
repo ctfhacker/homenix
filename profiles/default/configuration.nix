9d8be9e315deb82f56fb1e58fa34cc3ff7f8ccea
Cory Duplantis <cld251@gmail.com>
2024-06-05 14:10:43 -0500

    Add nix-ld

--- profiles/default/configuration.nix
+++ profiles/default/configuration.nix
@@ -148,6 +148,22 @@
     enable = true;
   };
 
+  programs.nix-ld = {
+    enable = true;
+
+    libraries = with pkgs; [
+      stdenv.cc.cc
+      zlib
+      fuse3
+      icu
+      zlib
+      nss
+      openssl
+      curl
+      expat
+    ];
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
